9bcbee36fa5dc7505d7d47ca1152dfcf8dd76646
SBDJAY <sbdjay183@gmail.com>
2026-02-09 14:34:12 -0500

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaOrderApi.Models;
+
+namespace PizzaOrderApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    //lambda function
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+}
